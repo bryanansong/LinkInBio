7d685d3cdfafe8249e32e072bed5ac6117de87f8
Bryan Ansong <50881304+bryanansong@users.noreply.github.com>
2024-08-15 18:33:05 -0400

    fixed height and spacing and removed twitter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,14 +14,16 @@ export default function Home() {
 		{ title: "TikTok", url: process.env.TIKTOK_URL || "..no tiktok.." },
 		{ title: "LinkedIn", url: process.env.LINKEDIN_URL || "..no linkedin.." },
 		{ title: "GitHub", url: process.env.GITHUB_URL || "..no github.." },
-		{ title: "Twitter", url: process.env.TWITTER_URL || "..no twitter.." },
 	];
 
 	return (
-		<main className="flex min-h-screen flex-col justify-center items-center w-[390px]">
-			<h1 className="w-full font-ultra text-start text-[32px] pb-8">
-				Welcome
-			</h1>
+		<main className="flex min-h-svh flex-col justify-center items-center w-[390px]">
+			<div className="w-full flex">
+				<h1 className="flex flex-1 w-full font-ultra text-end text-[32px] pb-8 ml-2">
+					Welcome
+				</h1>
+				<div className="flex flex-1" />
+			</div>
 			<ProfileSection
 				profilePicUrl={profilePicUrl}
 				profileDetails={profileDetails}
